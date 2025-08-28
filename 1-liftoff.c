78e539cee6ee68e5cbc8e4968dc27831163d8161
FoggyWonk <bdame08@gmail.com>
2025-08-28 14:31:53 -0500

    initial commit

---
+++ 1-liftoff.c
@@ -0,0 +1,40 @@
+/**
+ * Author: Brady Dame
+ * Date: 2025-08-28
+ * 
+ * This program displays some ASCII art of a rocket taking off
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv){
+
+    printf("\n\n\n\n\n");
+    printf(" _______________________________________________________________\n");
+    printf("/                                                 .             \\\n");
+    printf("|        .             GOODBYE WORLD                             |\n");
+    printf("|                                                                |\n");
+    printf("|                               *                              * |\n");
+    printf("|  *                   .                 .--.          +         |\n");
+    printf("|                                       / /  `                   |\n");
+    printf("|              +                       | |                       |\n");
+    printf("|                     .                 \\ \\__                    |\n");
+    printf("|                 *             +        `--`    *               |\n");
+    printf("|                     +    /\\                               .    |\n");
+    printf("|  +                     .`  `.       *                          |\n");
+    printf("|          .            /======\\               +                 |\n");
+    printf("|                      ;:.  _   ;                                |\n");
+    printf("|                      |:. (_)  |        .                       |\n");
+    printf("|                   *  |:.  _   |                                |\n");
+    printf("|      +               |:. (_)  |                         *      |\n");
+    printf("|                      ;:.      ;                                |\n");
+    printf("|                    .' \\:.    / `.            *                 |\n");
+    printf("|                   / .-'':._.'`-. \\                             |\n");
+    printf("|          *       |/     /||\\     \\|                   .        |\n");
+    printf("|  +             _..--'''`'`'`'`'''--.._                         |\n");
+    printf("|          _.-'``                       ``'-._       .           |\n");
+    printf("\\_______-'___________________________________'-_______________+_/\n\n\n\n\n\n\n\n");
+
+
+    return 0;
+}
